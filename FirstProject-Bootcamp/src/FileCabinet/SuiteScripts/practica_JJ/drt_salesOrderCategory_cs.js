94b18b07bc41c167540a498b1e508bff2a692e13
JUANDELUQUEZ <juandeluquez1123@gmail.com>
2026-09-09 11:05:42 -0600

    Creacion de ticket de categoria de cliente y documentacion.

--- FirstProject-Bootcamp/src/FileCabinet/SuiteScripts/practica_JJ/drt_salesOrderCategory_cs.js
+++ FirstProject-Bootcamp/src/FileCabinet/SuiteScripts/practica_JJ/drt_salesOrderCategory_cs.js
@@ -3,6 +3,7 @@
  * @NScriptType ClientScript
  * @NModuleScope SameAccount
  */
+// Modulos seleccionaos de NetSuite para el desarrollo del script.
 define(["N/runtime", "N/ui/dialog"] /**
  * @param{runtime} runtime
  * @param{dialog} dialog
@@ -33,18 +34,41 @@ define(["N/runtime", "N/ui/dialog"] /**
   function fieldChanged(context) {
     // desarrollo de ticket - categorias de cliente...
     try {
-      log.debug('fieldChanged context', context);
+      log.debug("fieldChanged context", context);
+      /*         // imprimimos en con sola el id del valor que el usuario selecciona en el campo
+        console.log("fieldValue", fieldValue); */
       // Declaramos una varaible para el registro actual que el usuario va a estar editando.
       var currentRecord = context.currentRecord;
       // Declaramos una variable para capturar el id del campo que se esta editanod.
       var fieldId = context.fieldId;
       // Construimos barrera condicional con el id del campo que queremos monitorear comparado con el id del campo que se esta editando.
-      if (fieldId === 'custentity_lead_category') {
-        // Declaramos una variable para capturar el valor del campo que se esta editando. 
-        const fieldValue = currentRecord.getValue({ fieldId: fieldId});
-        // 
+      if (fieldId === "custentity_lead_category") {
+        // Declaramos una variable para capturar el valor del campo que se esta editando.
+        const fieldValue = currentRecord.getValue({ fieldId: fieldId });
+        /*// imprimimos en con sola el id del valor que el usuario selecciona en el campo con...
+      // console.log("fieldValue", fieldValue); */
+
+        // Conicional para saber si el valor del campo es igual a 1 que son los valores de la categoria de cliente que nos interesa monitoreas.
+        if (fieldValue == 1) {
+          currentRecord.setValue({
+            fieldId: "custentity_drt_descripcion_credito_ct",
+            value:
+              "El cliente es de categoria A, - Pendiente revisión de crédito.",
+            // El parametro ignoreFieldChange evita que se dispare el evento fieldChanged nuevamente al cambiar el valor del campo.
+            ignoreFieldChange: true,
+          });
+          // Alerta de editar credito del cliente - mediante objeto usando el modulo seleccionado de netsuite dialog.
+          const options = {
+            title: "Alerta de credito para cliente de categoria A",
+            message:
+              "El cliente es de categoria A, - Pendiente revisión de crédito.",
+          };
+          dialog.alert(options);
+        }
       }
-    } catch (error) {}
+    } catch (error) {
+      log.error("Error en fieldChange", error);
+    }
   }
 
   /**
